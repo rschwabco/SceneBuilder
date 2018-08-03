a96c9f97f5e0d4a5ce84ef99161f1deb7bc6ca87
Leo Langinger <llanginger@gmail.com>
2018-08-03 13:47:50 -0700

    Cleaned up ship, added tab + space to trigger overlay

--- src/SceneViewer/Scenes/TankerScene.js
+++ src/SceneViewer/Scenes/TankerScene.js
@@ -6,7 +6,7 @@ import * as kkk from "aframe-event-set-component";
 import { Entity, Scene } from "aframe-react";
 import Camera from "../Camera"
 import assets from "../../assets/registerAssets"
-
+import { HtmlShader } from "../../DataOverlay/HtmlShader"
 import gql from "graphql-tag";
 import { getAssetsQuery } from "../../GraphQL"
 import { Query } from "react-apollo";
@@ -19,7 +19,21 @@ import {
     TouchableOpacity
 } from "react-native-web";
 
-
+const dummyShipProp = [
+    {
+        overlayText: [
+            "Propeller health: 95%",
+            "Engine health: 56%",
+            "Rudder health: 20% *"
+        ]
+    }, {
+        overlayText: [
+            "Propeller health: 25% *",
+            "Engine health: 99%",
+            "Rudder health: 82%"
+        ]
+    }
+]
 // Get array of positions and array of "Container" objects.
 const ContainerQuery = getAssetsQuery("TankerShip")
 
@@ -36,58 +50,171 @@ class ContainerScene extends Component {
             y: 0,
             z: -10,
             pinch: false,
-            assets: []
+            assets: [],
+            ships: [
+                {
+                    overlayText: [
+                        "Propeller health: 95%",
+                        "Rudder health: 20% *"
+                    ]
+                }, {
+                    overlayText: [
+                        "Propeller health: 25% *",
+                        "Engine health: 99%",
+                        "Rudder health: 82%"
+                    ]
+                }
+            ]
         };
 
 
     }
 
-    makeEntities = (data) => {
+    _makeContainerRow = (options) => {
 
-        const { obj, allPositions } = data
-        const { name, scale } = obj[0]
+        const { rowNumber, color = "#FFF", wireframe = false } = options
+        const containerWidth = 0.3
+        const platformWidth = 2
+        const rowZ = 0.75 * rowNumber
+        let containers = []
 
-        // Needs more work, but essentially;
-        // Map over allPositions and return an entity with the corresponding position and element
-        // Figure out how to rotate on different axis
 
-        return allPositions.map((position, i) => {
-            if (i === 0) {
+        for (let i = -0.83; containers.length < 6; i += (containerWidth + 0.03)) {
+            containers.push(
+                <a-box
+                    color={color}
+                    width={containerWidth}
+                    position={`${i} 1.155 ${-0.355 - rowZ}`}
+                    height=".3"
+                    depth=".7"
+                    wireframe={wireframe}
+                >
+                </a-box>
+            )
+        }
 
-                return (
-                    <a-entity click-drag key={i} position={`${0} ${0} ${0}`} scale={`${scale} ${scale} ${scale} `} rotation="0 225 0" obj-model={`obj: #${name}-obj; mtl: #${name}-mtl;`} >
-
-                    </a-entity>
-                )
-            }
-            return
-        })
+        return containers
     }
+
+    _makeShips = (options) => {
+
+        return options.map((ship, i) => {
+            // if (i === 0) { // Uncomment to test just 1 ship
+
+            return (
+                <a-entity
+                    position={`${i * 3} 0 -6`}
+                    log-info
+                >
+                    <a-sphere
+                        // hover-info
+                        id={`ship-${i}`}
+                        color="#FFF"
+                        theta-length="90"
+                        phi-length="180"
+                        rotation="180 180 0"
+                        position="0 1 0"
+                        wireframe={false}
+                        side="double"
+                    // wireframe="true"
+                    ></a-sphere>
+                    <a-ring
+                        color="#FFF"
+                        radius-inner=".001"
+                        radius-outer="1"
+                        position="0 .995 0"
+                        rotation="270 0 0"
+                    ></a-ring>
+                    <a-cylinder
+                        side="double"
+                        color="#FFF"
+                        theta-length="180"
+                        theta-start="270"
+                        height="3.5"
+                        position="0 1 -1.75"
+                        rotation="90 0 0"
+                        wireframe={false}
+                    ></a-cylinder>
+                    <a-sphere
+                        side="double"
+                        color="#FFF"
+                        theta-length="90"
+                        phi-length="180"
+                        rotation="90 180 0"
+                        position="0 1 -3.5"
+                        wireframe={false}
+                    >
+                    </a-sphere>
+                    <a-ring
+                        color="#FFF"
+                        radius-inner=".001"
+                        radius-outer="1"
+                        position="0 .995 -3.5"
+                        rotation="270 0 0"
+                    ></a-ring>
+                    <a-box
+                        side="double"
+                        color="#FFF"
+                        position="0 0.975 -1.75"
+                        depth="3.5"
+                        width="2"
+                        height="0.05"
+                        wireframe={false}
+                    >
+                    </a-box>
+
+                    {this._makeContainerRow({ rowNumber: 0 })}
+                    {this._makeContainerRow({ rowNumber: 1, color: "#FFF", wireframe: false })}
+                    {this._makeContainerRow({ rowNumber: 2, color: "#FFF", wireframe: true })}
+                    {this._makeContainerRow({ rowNumber: 3, color: "#FFF", wireframe: false })}
+                    <a-box
+                        color="#FFF"
+                        depth=".3"
+                        height=".8"
+                        width="1"
+                        position="0 1.4 -3.258"
+                    ></a-box>
+                    <a-box
+                        color="#FFF"
+                        depth=".3"
+                        height=".2"
+                        width="0.2"
+                        position="-.6 1.7 -3.258"
+                    ></a-box>
+                    <a-box
+                        color="#FFF"
+                        depth=".3"
+                        height=".2"
+                        width="0.2"
+                        position=".6 1.7 -3.258"
+                    ></a-box>
+                    {this.props.showInfoModal &&
+                        <HtmlShader id={i} overlayText={ship.overlayText} position={{ x: 2, y: 2, z: -2 }}
+                        />
+                    }
+                </a-entity>
+            )
+            // }
+        })
+
+    }
+
 
     render() {
         let { pitch, roll, yaw, x, y, z } = this.state;
+
+
         return (
             <Query query={ContainerQuery}>
                 {({ loading, error, data }) => {
-
-                    console.log("Data: ", data)
 
                     if (loading) return <ActivityIndicator color={"#fff"} />;
                     if (error) return <Text>{`Error: ${error}`}</Text>;
 
                     return (
-                        <Scene vr-mode-ui keyboard-shortcuts leap="vr: false">
-
-                            {/* Map over all (in this case just one) assets listed in GQL query and register said assets with a-frame */}
-                            {assets(data.obj, data.mtl)}
-
-                            <Entity>
-                                <Camera />
-                                {this.props.children}
-                                {this.makeEntities(data)}
-                                <a-sky src="#sky" rotation="0 -270 0" />
-                            </Entity>
-                        </Scene>
+                        <a-entity>
+                            {this._makeShips(this.state.ships)}
+                        </a-entity>
                     )
                 }}
             </Query>
