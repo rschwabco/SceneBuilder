8b6bd23a83f61163fb5f06d3c3c0779014512e60
Leo Langinger <llanginger@gmail.com>
2018-08-21 22:46:43 -0700

    nav buttons more polished, added PQs to scenes

--- src/SceneViewer/Prefabs/checkpoints.js
+++ src/SceneViewer/Prefabs/checkpoints.js
@@ -7,18 +7,26 @@ export const makeCheckpoints = (props, node) => {
         const { id, containerNode } = point
         const { position } = containerNode
         const { x, y, z } = position
-        const dim = 0.3
+        const dim = 0.6
         return (
             <a-box
                 className="checkpoint-box"
                 camera-to-here={`cameraTo: ${0 + x} ${4 + y} ${5 + z}; lookAt: #${id}`}
-                color={`orangered`}
-                position={`${6} ${(i * 3) + 1.5} 3`}
+                color="transparent"
+                opacity="0.3"
+                position={`${i === 0 ? 6 : -6} ${-1} 3`}
                 height={`${dim}`}
                 width={`${dim}`}
                 depth={`${dim}`}
             >
-                <a-text position="0 1 0" value={`Click to show part# ${id}`}></a-text>
+                <a-box
+                    wireframe
+                    position="0 0 0"
+                    height={`${dim}`}
+                    width={`${dim}`}
+                    depth={`${dim}`}
+                ></a-box>
+                <a-text align="center" color="white" position="0 0 0" value={i === 0 ? "Next" : "Last"}></a-text>
             </a-box>
         )
     })
